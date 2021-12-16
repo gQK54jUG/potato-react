11bc6649112788bf8054280d4e9bbe89d3b87df4
GoTaku <takuma_goda.05@icloud.com>
2021-12-16 17:40:49 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Input extends React.Component {
+    render() {
+        return(
+            <form>
+                <div className='input textarea'>
+                    <textarea></textarea>
+                </div>
+                <div className='submit'>
+                    <input type='submit' value='投稿する'/>
+                </div>
+            </form>
+        );
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Bulletin extends React.Component {
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+class Top extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            log: '',
+            persons: []
+        };
+    }
+
+    apiCheck() {
+        this.setState({
+            log: 'ko'
+        });
+        console.log(this.state.log);
+        // axios.get(`https://jsonplaceholder.typicode.com/users`)
+        //     .then(res => {
+        //         const persons = res.data;
+        //         this.setState({persons});
+        //     });
+        
+        // this.state.persons.map((val, key) => {
+        //     console.log(key, val);
+        // });
+    }
+
+    render() {
+        var textareaCheck = '';
+
+        // this.apiCheck();
+
+        return (
+            <div className='top-nav-title'>
+                <div>
+                    <a href='/'>
+                        <span>Potato BB🥔</span>
+                    </a>
+                </div>
+                <div>
+                    {textareaCheck}
+                </div>
+                <Input />
+                <Bulletin />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Top />, document.getElementById('root'));
